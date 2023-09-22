9bcd8c8e5510d5f76f2a4b1285bf960c31fdf553
Gian Luca <55563130+GLPG35@users.noreply.github.com>
2023-09-22 15:26:49 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,60 @@
 const button = document.querySelector('button')
 const result = document.querySelector('.result')
 
+const types = {
+	'Láminas': [
+		'láminas',
+		'lámina',
+	],
+	'Envíos': [
+		'envíos',
+		'envío'
+	],
+	'Discos': [
+		'discos',
+		'disco'
+	]
+}
+
 button.addEventListener('click', () => {
 	navigator.clipboard.readText()
 	.then(text => {
-		const prices = text.match(/(\d+\d{1,2})/g)
+		const prices = text.split('\n')
+		const parsePrices = prices.map(product => {
+			const price = Number(product.match(/(?<=\$)(.*)/gi))
+			const type = product.split(' - ')[0].split(' ')[1]
 
-		const total = prices.reduce((prev, curr) => +prev + +curr, 0)
+			return {
+				price,
+				type
+			}
+		})
+
+		const total = parsePrices.reduce((prev, curr) => prev + curr.price, 0)
+		const parsedTypes = {
+			'Láminas': 0,
+			'Envíos': 0,
+			'Discos': 0
+		}
+		parsePrices.forEach(price => {
+			const type = Object.entries(types).find(x => x[1].find(y => y.localeCompare(price.type, undefined, { sensitivity: 'base' }) === 0))
+
+			parsedTypes[type[0]] += price.price
+		})
 
 		result.innerHTML = `$${total} ✨`
+		result.insertAdjacentHTML('afterend', `
+			<div class="types">
+				<div class="type">
+					<i class="ph-bold ph-file"></i>$${parsedTypes['Láminas']}
+				</div>
+				<div class="type">
+					<i class="ph-bold ph-truck"></i>$${parsedTypes['Envíos']}
+				</div>
+				<div class="type">
+					<i class="ph-bold ph-disc"></i>$${parsedTypes['Discos']}
+				</div>
+			</div>
+		`)
 	})
 })
