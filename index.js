ac6297fb255931ebc061e60b585ceeef4674194f
Gian Luca <55563130+GLPG35@users.noreply.github.com>
2024-06-17 21:29:08 -0300

    v2.0.0
    - Add color palettes based on current month
    - Add new UI for improved readability
    - Add new category for common savings
    - Add the ability to download screenshots from the web itself

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const button = document.querySelector('button')
 const result = document.querySelector('.result')
 const typesContainer = document.querySelector('.types')
+const downloads = document.querySelector('.download')
+const capture = document.querySelector('.capture')
+
+const currentMonth = Intl.DateTimeFormat('en-US', { month: 'long' }).format(Date.now()).toLocaleLowerCase()
+const currentPalette = colorPalettes[currentMonth]
+document.documentElement.style.setProperty('--bg-color', currentPalette.bg)
+document.documentElement.style.setProperty('--button-color', currentPalette.button)
+document.documentElement.style.setProperty('--card-color', currentPalette.card)
+document.documentElement.style.setProperty('--text-color', currentPalette.text)
 
 const types = {
 	'Láminas': [
@@ -28,10 +37,14 @@ button.addEventListener('click', () => {
 		const parsePrices = prices.map(product => {
 			const price = Number(product.match(/(?<=\$)(.*)/gi))
 			const type = product.split(' - ')[0].split(' ')[1]
+			const quantity = product.split(' ')[0]
+			const wpp = product.split(' - ')[0].split(' ').length === 3 ? true : false
 
 			return {
 				price,
-				type
+				type,
+				quantity,
+				wpp
 			}
 		})
 
@@ -42,26 +55,76 @@ button.addEventListener('click', () => {
 			'Discos': 0,
 			'Impresiones': 0
 		}
+		let savings = 0
 		parsePrices.forEach(price => {
 			const type = Object.entries(types).find(x => x[1].find(y => y.localeCompare(price.type, undefined, { sensitivity: 'base' }) === 0))
+			
+			if (type[0] == 'Láminas') {
+				savings += price.wpp ? 100 * price.quantity : 58 * price.quantity
+			}
 
 			parsedTypes[type[0]] += price.price
 		})
 
-		result.innerHTML = `$${total} ✨`
-		typesContainer.innerHTML = `
-			<div class="type">
-				<i class="ph-bold ph-file"></i>$${parsedTypes['Láminas']}
-			</div>
-			<div class="type">
-				<i class="ph-bold ph-truck"></i>$${parsedTypes['Envíos']}
-			</div>
-			<div class="type">
-				<i class="ph-bold ph-disc"></i>$${parsedTypes['Discos']}
-			</div>
-			<div class="type">
-				<i class="ph-bold ph-printer"></i>$${parsedTypes['Impresiones']}
+		result.innerHTML = `
+			<i class="ph-light ph-star-four"></i>
+			<div class="content">
+				<div class="title">Total Gian</div>
+				<span>$${total}</span>
 			</div>
 		`
+		typesContainer.innerHTML = `
+			<div class="type">
+				<i class="ph-light ph-bank"></i>
+				<div class="title">Ahorros</div>
+				<span>$${savings}</span>
+			</div>
+			<div class="type">
+				<i class="ph-light ph-file"></i>
+				<div class="title">Láminas</div>
+				<span>$${parsedTypes['Láminas']}</span>
+			</div>
+			<div class="type">
+				<i class="ph-light ph-truck"></i>
+				<div class="title">Envíos</div>
+				<span>$${parsedTypes['Envíos']}</span>
+			</div>
+			<div class="type">
+				<i class="ph-light ph-disc"></i>
+				<div class="title">Discos</div>
+				<span>$${parsedTypes['Discos']}</span>
+			</div>
+			<div class="type">
+				<i class="ph-light ph-printer"></i>
+				<div class="title">Impresiones</div>
+				<span>$${parsedTypes['Impresiones']}</span>
+			</div>
+		`
+
+		downloads.innerHTML = `
+			<button class="all">
+				<div class="icon">
+					<i class="ph-bold ph-file-arrow-down"></i>
+				</div>
+				Descargar todo
+			</button>
+		`
+
+		const downloadAllButton = document.querySelector('button.all')
+
+		const downloadAll = () => {
+			html2canvas(capture).then(canvas => {
+				const imgData = canvas.toDataURL('image/png')
+				const currentMonth = new Intl.DateTimeFormat('es-UY', { month: 'long' }).format(Date.now())
+
+				const link = document.createElement('a')
+				link.download = `Todo Láminas ${currentMonth}.png`
+				link.href = imgData
+				link.click()
+				link.remove()
+			})
+		}
+
+		downloadAllButton.addEventListener('click', downloadAll)
 	})
 })
---
+++ palettes.js
@@ -0,0 +1,74 @@
+const colorPalettes = {
+	january: {
+		bg: '#fce9e9',
+		button: '#fad0d0',
+		card: '#ffabab',
+		text: '#b02c2c'
+	},
+	february: {
+		bg: '#fcede9',
+		button: '#fad8d0',
+		card: '#ffbaab',
+		text: '#b04b2c'
+	},
+	march: {
+		bg: '#fcf5e9',
+		button: '#faead0',
+		card: '#ffdcab',
+		text: '#e47f2c'
+	},
+	april: {
+		bg: '#fcfae9',
+		button: '#fbf3b4',
+		card: '#fef18f',
+		text: '#cc951d'
+	},
+	may: {
+		bg: '#eafce9',
+		button: '#bbfbb4',
+		card: '#95fe8f',
+		text: '#1f7e2f'
+	},
+	june: {
+		bg: '#edf2fb',
+		button: '#d0ddfa',
+		card: '#abc4ff',
+		text: '#2c54b0'
+	},
+	july: {
+		bg: '#ededfb',
+		button: '#b8b4fb',
+		card: '#938ffe',
+		text: '#2f2d67'
+	},
+	august: {
+		bg: '#f5edfb',
+		button: '#e1b4fb',
+		card: '#c78ffe',
+		text: '#542a71'
+	},
+	september: {
+		bg: '#fce9f4',
+		button: '#fad0e8',
+		card: '#ffabdf',
+		text: '#b02c93'
+	},
+	october: {
+		bg: '#2e2a27',
+		button: '#ed7b4b',
+		card: '#ea6228',
+		text: '#ffe6d4'
+	},
+	november: {
+		bg: '#fce9f0',
+		button: '#fad0dd',
+		card: '#ffabc7',
+		text: '#b02c54'
+	},
+	december: {
+		bg: '#faefef',
+		button: '#f3afb1',
+		card: '#add99b',
+		text: '#894a4a'
+	}
+}
